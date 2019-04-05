7bfcfffb1bd543184962f1565f8362dbef2cecef
jackil182 <jackil182@gmail.com>
2019-04-05 09:50:09 +0300

    added route to save orders, check if product exists in db, if so, save the order file

--- Homework-3/src/routes/OrderRoute/order-route.js
+++ Homework-3/src/routes/OrderRoute/order-route.js
@@ -3,19 +3,37 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const findProducts = order => {
+  const {products} = order;
+  const allProductsPath = path.join(__dirname, '../../db/all-products.json');
+  const allProducts = JSON.parse(fs.readFileSync(allProductsPath));
+  const orderedProducts = products.map(el => allProducts.filter(x => x.id === el));
+  return orderedProducts;
+}
+
 const saveOrder = order => {
-  const orderId = order.user;
-  const filePath = path.join(__dirname, '../../db/orders', `${orderId}.json`);
-  fs.writeFile(filePath, JSON.stringify(order), err => {
-    if (err) throw err;
-    console.log('order saved');
-  });
+  const orderedProducts = findProducts(order);
+  const canBeFulfilled = orderedProducts.every(el => el.length > 0);
+  if(canBeFulfilled) {
+    const orderId = order.user;
+    const filePath = path.join(__dirname, '../../db/orders', `${orderId}.json`);
+    fs.writeFile(filePath, JSON.stringify(order), err => {
+      if (err) throw err;
+      console.log('order saved');
+    });
+    return orderedProducts;
+  } else return null;
 };
 
 router.post('/', (req, res) => {
-  console.log('body', req.body)
-  saveOrder(req.body);
-  res.send({status: "success", order: req.body});
+  // console.log('body', req.body)
+  const order = saveOrder(req.body);
+  req.body.products = order.map(el => el[0]);
+  if(order) {
+    res.send({status: "success", order: req.body});
+  } else {
+    res.send({status: "failed", order: null});
+  }
 });
 
 module.exports = router;
